c9e35349441534dfd9cd2b81e5bb22566888bff4
luisregardiz <gadeeks@gmail.com>
2025-08-20 20:32:28 -0400

    feat: add netlify app URL to CORS allowed origins

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
 
   // Enable CORS for specific origins
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://burger-client-luis.netlify.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
